f4b6d5ebefe7a06ec0c854eb456ae7f6b7d55744
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-02-04 13:36:40 -0700

    started methods for panel

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -9,8 +9,35 @@ public class PokedexPanel extends JPanel
 	private SpringLayout appLayout;
 	private JTextField textField;
 	private JLabel labels;
+	
 	public PokedexPanel(PokedexController appController)
 	{
 		super();
+		
+		this.appController = appController;
+		this.appLayout = new SpringLayout();
+		this.textField = new JTextField();
+		this.labels = new JLabel("I wanna be the very best");
+		
+		setupLayout();
+		setupPanel();
+		setupListeners();
+	}
+	
+	private void setupLayout()
+	{
+		//insert code dump here
+	}
+	
+	private void setupPanel()
+	{
+		this.add(labels);
+		this.add(textField);
+	}
+	
+	private void setupListeners()
+	{
+		
+		
 	}
 }
